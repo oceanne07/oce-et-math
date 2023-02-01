ec24bf91c53d75cdd416d86108d4155335c7e52f
oceanne07 <122922124+oceanne07@users.noreply.github.com>
2023-02-01 15:30:55 -0500

    Mise à jour pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -179,26 +179,6 @@ game.onUpdateInterval(375, function () {
         `, SpriteKind.Projectile)
     projectile.setVelocity(0, 100)
     projectile.setPosition(randint(0, 120), 0)
-})
-game.onUpdateInterval(500, function () {
     auto.setBounceOnWall(true)
-    projectile = sprites.createProjectileFromSprite(img`
-        . . . . . . e e c c e e . . . . 
-        . . . . . e 2 2 2 2 2 2 e . . . 
-        . . . . 2 c 2 2 2 2 2 2 c 2 . . 
-        . . . e 2 c 4 2 2 2 2 2 c 2 e . 
-        . . . f 2 2 4 2 2 2 2 2 c 2 f . 
-        . . . f 2 2 4 2 2 2 2 2 2 2 f . 
-        . . . f 2 2 4 2 2 2 2 2 2 2 f . 
-        . . . f 2 c 2 4 4 2 2 2 c 2 f . 
-        . . . e 2 c e c c c c e c 2 e . 
-        . . . e 2 e c b b b b c e 2 e . 
-        . . . e 2 e b b b b b b e 2 e . 
-        . . . e e e e e e e e e e e e . 
-        . . . f e d e e e e e e d e f . 
-        . . . f e 2 d e e e e d 2 e f . 
-        . . . f f e e e e e e e e f f . 
-        . . . . f f . . . . . . f f . . 
-        `, projectile, 50, 50)
-    projectile.setPosition(randint(0, 120), 180)
+    projectile.setFlag(SpriteFlag.DestroyOnWall, true)
 })
